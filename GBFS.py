445d120dff948f9a3019955aa17cd6f735b36143
Harry1212 <118365419+Harry1212159@users.noreply.github.com>
2024-07-22 10:58:34 +0700

    modify GBFS

--- GBFS.py
+++ GBFS.py
@@ -1,58 +1,36 @@
 import heapq
 
-class Graph:
-    def __init__(self, nodes, matrix, heuristics, start, end):
-        self.nodes = nodes
-        self.matrix = matrix
-        self.heuristics = heuristics
-        self.start = start
-        self.end = end
+# manhattan distance
+def heuristics(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-def GBFS(graph):
-    pq = []
-    parent = {}
-
-    heapq.heappush(pq, (graph.heuristics[graph.start], graph.start))
-    parent[graph.start] = None
+def GBFS(matrix, start, goal):
+    n = len(matrix)
+    m = len(matrix[0])
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     
-    while pq:
-        current_heuristic, current_node = heapq.heappop(pq)
-
-        if(current_node == graph.end):
+    explored_node = []
+    heapq.heappush(explored_node, (0, start))
+    came_from = {}
+    came_from[start] = None
+    
+    while explored_node:
+        _, current = heapq.heappop(explored_node)
+        
+        if current == goal:
             path = []
-            while current_node is not None:
-                path.append(current_node)
-                current_node = parent[current_node]
+            while current:
+                path.append(current)
+                current = came_from[current]
             path.reverse()
             return path
         
-        for i in range(graph.nodes):
-            if graph.matrix[current_node][i] != 0 and i not in parent:
-                heapq.heappush(pq, (graph.heuristics[i],i))
-                parent[i] = current_node
-    
-    return [-1]
+        for direction in directions:
+            neighbor = (current[0] + direction[0], current[1] + direction[1])
+            if 0 <= neighbor[0] < n and 0 <= neighbor[1] < m and matrix[neighbor[0]][neighbor[1]] != '-1':
+                if neighbor not in came_from:
+                    priority = heuristics(neighbor, goal)
+                    heapq.heappush(explored_node, (priority, neighbor))
+                    came_from[neighbor] = current
 
-
-if __name__ == "__main__":
-    nodes = 10
-    matrix = [
-        [0, 0, 0, 0, 0, 0, 5, 3, 3, 4],
-        [0, 0, 3, 5, 0, 0, 0, 0, 3, 0],
-        [0, 3, 0, 0, 3, 0, 0, 0, 0, 2],
-        [0, 5, 0, 0, 0, 5, 0, 0, 0, 0],
-        [0, 0, 3, 0, 0, 1, 5, 2, 0, 0],
-        [0, 0, 0, 5, 1, 0, 5, 0, 4, 0],
-        [5, 0, 0, 0, 5, 5, 0, 0, 0, 0],
-        [3, 0, 0, 0, 2, 0, 0, 0, 0, 0],
-        [3, 3, 0, 0, 0, 4, 0, 0, 0, 0],
-        [4, 0, 2, 0, 0, 0, 0, 0, 0, 0]
-    ]
-    heuristics = [3, 15, 12, 0, 6, 9, 3, 6, 18, 24]
-    start = 0
-    end = 3
-
-    graph = Graph(nodes, matrix, heuristics, start, end)
-    path = GBFS(graph)
-    
-    print("Path:", " -> ".join(map(str, path)))
+    return []
